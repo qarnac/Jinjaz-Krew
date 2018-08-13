49468988518ded49b7268bd991683c16f74bc7b9
dannyg185 <42353365+dannyg185@users.noreply.github.com>
2018-08-13 14:37:47 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import webapp2
+import jinja2
+import os
+import logging
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class FoodType(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        logging.info('GetFoodType')
+
+        mypage = env.get_template('templates/food.html')
+        self.response.write(mypage.render())
+    def post(self):
+        logging.info('PostFoodType')
+        mypage = env.get_template('templates/food.html')
+        self.response.write(mypage.render())
+
+class FoodInfo(webapp2.RequestHandler):
+    def get(self):
+        logging.info('GetFoodInfo')
+        mypage = env.get_template('templates/foodtype.html')
+        self.response.write(mypage.render())
+
+    def post(self):
+        logging.info('PostFoodInfo')
+        mypage = env.get_template('templates/foodtype.html')
+        self.response.write(mypage.render())
+
+app = webapp2.WSGIApplication([
+    ('/', FoodType),
+    ('/', FoodInfo)
+    #('/', )
+], debug=True)
