b1a5dbb76010fd366947e5c711570ca7351a9ee2
Ace Panapuzha <pvsaks@hotmail.com>
2018-08-15 11:37:58 -0700

    Added the ability to exclude more than one item

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ def getUrl(foodName,restrictionsList):
     # restrictionsList, caloriesMin, caloriesMax, healthInfo
     food_name = 'q=' + foodName
     restrictions = ""
-    for i in restrictionsList:
-        restrictions += '&exclude=' + restrictionsList[i]
+    for item in restrictionsList:
+        restrictions += '&exclude=' + item
     #maxCalories = caloriesMax
     #minCalories = caloriesMin
 #    health_restrictions = healthInfo
-    
+
     url = 'https://api.edamam.com/search?app_id=28c73d69&app_key=b4dc98f7d320f1968ef7b63205e2e462&' + food_name + restrictions
     try:
         result = urlfetch.fetch(url)
