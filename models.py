ba341c4aee912c7a79690ceaa965fbff234b0338
Ace Panapuzha <pvsaks@hotmail.com>
2018-08-15 11:18:06 -0700

    store before adding new changes

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ def getUrl(foodName,restrictionsList):
     #maxCalories = caloriesMax
     #minCalories = caloriesMin
 #    health_restrictions = healthInfo
-
+    
     url = 'https://api.edamam.com/search?app_id=28c73d69&app_key=b4dc98f7d320f1968ef7b63205e2e462&' + food_name + restrictions
     try:
         result = urlfetch.fetch(url)
