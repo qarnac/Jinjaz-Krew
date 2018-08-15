19bcaa815311198d2a61ba0b109c8b9086c93396
Ace Panapuzha <pvsaks@hotmail.com>
2018-08-15 16:17:16 -0700

    Day 3 end

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import jinja2
 import os
 import logging
 from models import getUrl
+import json
+import urllib2
+from google.appengine.api import *
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -17,10 +20,21 @@ class MainPage(webapp2.RequestHandler):
         foodName = self.request.get('foodName')
         restriction = self.request.POST.getall('restrictions')
         health = self.request.POST.getall('health')
-        json_object = getUrl(foodName,restriction,health)
+        url = getUrl(foodName,restriction,health)
 
-        #mypage = env.get_template('templates/randomfood.html')
-        self.response.write(json_object)
+        try:
+            result = urlfetch.fetch(url)
+            if result.status_code == 200:
+                json_object = json.loads(result.content)
+                logging.info(json_object['hits']['recipe'])
+
+                mypage = env.get_template('templates/randomfood.html')
+                self.response.write(json_object)
+            else:
+                self.response.status_code = result.status_code
+        except urlfetch.Error:
+            logging.exception('Caught exception fetching url')
+        #
 class RandomPage(webapp2.RequestHandler):
     def get(self):
         mypage = env.get_template('templates/randomfood.html')
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import urllib2
 import json
 import logging
-from google.appengine.api import *
+
 
 def getUrl(foodName,restrictionsList,healthList):
     # restrictionsList, caloriesMin, caloriesMax, healthInfo
@@ -18,12 +18,4 @@ def getUrl(foodName,restrictionsList,healthList):
 #    health_restrictions = healthInfo
 
     url = 'https://api.edamam.com/search?app_id=28c73d69&app_key=b4dc98f7d320f1968ef7b63205e2e462&' + food_name + restrictions + health
-    try:
-        result = urlfetch.fetch(url)
-        if result.status_code == 200:
-            data = result.content
-            return data
-        else:
-            self.response.status_code = result.status_code
-    except urlfetch.Error:
-        logging.exception('Caught exception fetching url')
+    return url
