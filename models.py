ca0b70bc5cd03f0f563bad517701b22ceda2ddd7
Ace Panapuzha <pvsaks@hotmail.com>
2018-08-15 10:50:23 -0700

    Work i had to push before I could pull the new background

--- models.py
+++ models.py
@@ -1,14 +1,18 @@
 import urllib2
 import json
+from google.appengine.api import *
 
-def getUrl(foodName, restrictionsList, caloriesMin, caloriesMax, healthInfo):
-    food_name = foodName
-    restrictions = restrictionsList
-    maxCalories = caloriesMax
-    minCalories = caloriesMin
-    health_restrictions = healthInfo
+def getUrl(foodName,restrictionsList):
+    # restrictionsList, caloriesMin, caloriesMax, healthInfo
+    food_name = 'q=' + foodName
+    restrictions = ""
+    for i in restrictionsList
+        restrictions += '&exclude=' + restrictionsList[i]
+    #maxCalories = caloriesMax
+    #minCalories = caloriesMin
+#    health_restrictions = healthInfo
 
-    url = 'https://api.edamam.com/search?app_id=28c73d69&app_key=b4dc98f7d320f1968ef7b63205e2e462&q=steak'
+    url = 'https://api.edamam.com/search?app_id=28c73d69&app_key=b4dc98f7d320f1968ef7b63205e2e462&' + food_name + restrictions
     try:
         result = urlfetch.fetch(url)
         if result.status_code == 200:
