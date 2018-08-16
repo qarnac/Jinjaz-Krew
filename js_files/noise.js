ca5eefc14cf52f02fc6d21ea13a4d2a89c92edd3
Karla Saldivar <saldi023@csusm.edu>
2018-08-16 11:49:17 -0700

    took out sound cause i cant do it )):

--- js_files/noise.js
+++
@@ -1,9 +0,0 @@
-let enter = document.getElementById('enter')
-
-
-enter.addEventListener('click', (e)=>{
-  console.log("Ding");
-  var sound = new Audio('../sound/sound.mp3');
-  if(sound === true);
-    sound.play();
-})
